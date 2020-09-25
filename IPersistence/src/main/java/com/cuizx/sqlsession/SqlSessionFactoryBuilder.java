2d21796e4572ec35dd1710b6243fad1f771a8071
cuizhongxi <cuizx@jidemail.com>
2020-09-25 16:54:29 +0800

    fix bug

--- IPersistence/src/main/java/com/cuizx/sqlsession/SqlSessionFactoryBuilder.java
+++ IPersistence/src/main/java/com/cuizx/sqlsession/SqlSessionFactoryBuilder.java
@@ -1,6 +1,6 @@
 package com.cuizx.sqlsession;
 
-import com.cuizx.config.XMLConfigureBuilder;
+import com.cuizx.config.XMLConfigBuilder;
 import com.cuizx.pojo.Configuration;
 import org.dom4j.DocumentException;
 
@@ -10,7 +10,7 @@ import java.io.InputStream;
 public class SqlSessionFactoryBuilder {
     public SqlSessionFactory build(InputStream in) throws PropertyVetoException, DocumentException {
         //第一：使用dom4j解析配置文件，将解析出来的内容封装到Configuration中
-        Configuration configuration=new XMLConfigureBuilder().parseConfigure(in);
+        Configuration configuration=new XMLConfigBuilder().parseConfigure(in);
 
 
         //创建sqlSessionFactory对象
